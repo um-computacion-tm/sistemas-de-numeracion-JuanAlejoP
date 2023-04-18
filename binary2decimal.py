1461c37106e9636cd72f9f58dc71ca2d73b43e7d
JuanAlejoP <j.patino@alumno.um.edu.ar>
2023-04-18 08:09:51 -0300

    Actualización menor.

--- binary2decimal.py
+++ binary2decimal.py
@@ -2,18 +2,15 @@ def binary_to_decimal(binary):
     
 	result = {}
 	for letter in binary:
-	if letter in result:
-		result[letter] +=1
-	else:
-		result[letter] = 1
-	return result
+		if letter in result:
+			result[letter] +=1
+		else:
+			result[letter] = 1
+		return result
 
-
-    return
 
 
 def decimal_to_binary(decimal):
-
 
     
     return
