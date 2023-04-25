bcbf7e9f261fbb926a8b273f167e7cd531011a5a
JuanAlejoP <j.patino@alumno.um.edu.ar>
2023-04-24 23:36:50 -0300

    Segunda verisón código y test.

--- binary2decimal.py
+++ binary2decimal.py
@@ -1,16 +1,139 @@
 def binary_to_decimal(binary):
     
-	result = {}
-	for letter in binary:
-		if letter in result:
-			result[letter] +=1
-		else:
-			result[letter] = 1
-		return result
+    decimal = 0
+    binary = binary[::-1]
 
+    for i in range(len(binary)):
+        if binary[i] == '1':
+            decimal += 2 ** i
+
+    return decimal
+
+
+def binary_to_octal(binary):
+
+    decimal = binary_to_decimal(binary)
+    octal = ''
+
+    while decimal > 0:
+        octal = str(decimal % 8) + octal
+        decimal //= 8
+
+    return octal
+
+
+def binary_to_hexadecimal(binary):
+    
+    decimal = binary_to_decimal(binary)
+    hexadecimal_digits = '0123456789ABCDEF'
+    hexadecimal = ''
+
+    while decimal > 0:
+        remainder = decimal % 16
+        hexadecimal = hexadecimal_digits[remainder] + hexadecimal
+        decimal //= 16
+
+    return hexadecimal
 
 
 def decimal_to_binary(decimal):
 
+    binary = ''
+
+    while decimal > 0:
+        binary = str(decimal % 2) + binary
+        decimal //= 2
+
+    return binary
+
+
+def decimal_to_octal(decimal):
     
-    return
+    octal = ''
+
+    while decimal > 0:
+        octal = str(decimal % 8) + octal
+        decimal //= 8
+
+    return octal
+
+
+def decimal_to_hexadecimal(decimal):
+
+    hexadecimal_digits = '0123456789ABCDEF'
+    hexadecimal = ''
+
+    while decimal > 0:
+        remainder = decimal % 16
+        hexadecimal = hexadecimal_digits[remainder] + hexadecimal
+        decimal //= 16
+
+    return hexadecimal
+
+
+def octal_to_decimal(octal):
+    
+    decimal = 0
+    octal = str(octal)[::-1]
+
+    for i in range(len(octal)):
+        decimal += int(octal[i]) * 8 ** i
+
+    return decimal
+
+
+def octal_to_binary(octal):
+    
+    decimal = octal_to_decimal(octal)
+    binary = ''
+    while decimal > 0:
+        binary = str(decimal % 2) + binary
+        decimal //= 2
+        
+    return binary
+
+
+def octal_to_hexadecimal(octal):
+    
+    decimal = octal_to_decimal(octal)
+    hexadecimal_digits = '0123456789ABCDEF'
+    hexadecimal = ''
+    
+    while decimal > 0:
+        remainder = decimal % 16
+        hexadecimal = hexadecimal_digits[remainder] + hexadecimal
+        decimal //= 16
+        
+    return hexadecimal
+
+
+def hexadecimal_to_decimal(hexadecimal):
+    
+    decimal = 0
+    hexadecimal = hexadecimal[::-1]
+    hexadecimal_digits = '0123456789ABCDEF'
+    
+    for i in range(len(hexadecimal)):
+        decimal += hexadecimal_digits.index(hexadecimal[i]) * 16 ** i
+        
+    return decimal
+
+
+def hexadecimal_to_binary(hexadecimal):
+    
+    decimal = hexadecimal_to_decimal(hexadecimal)
+    binary = ''
+    
+    while decimal > 0:
+        binary = str(decimal % 2) + binary
+        decimal //= 2
+        
+    return binary
+
+
+def hexadecimal_to_octal(hexadecimal):
+    
+    decimal = int(hexadecimal, 16)
+    octal = decimal_to_octal(decimal)
+	
+    return octal
--- test.py
+++ test.py
@@ -1,13 +1,68 @@
 import unittest
-from binary2decimal import binary_to_decimal
-from binary2decimal import decimal_to_binary
+from binary2decimal import binary_to_decimal, binary_to_octal, binary_to_hexadecimal, decimal_to_binary, decimal_to_octal, decimal_to_hexadecimal, octal_to_binary, octal_to_decimal, octal_to_hexadecimal, hexadecimal_to_binary, hexadecimal_to_decimal, hexadecimal_to_octal
 
-class TestNumeración(unittest.TestCase):
-    def testBinario2Decimal(self):
-        self.assertEqual(binary_to_decimal(('01011100'),92))
 
-    def testDecimal2Binario(self):
-        self.assertEqual(decimal_to_binary((97),'01100001'))
+class TestNumerción(unittest.TestCase):
+    
+    def test_binary_to_decimal(self):
+        self.assertEqual(binary_to_decimal('101010'), 42)
+        self.assertEqual(binary_to_decimal('11111111'), 255)
+        self.assertEqual(binary_to_decimal('0'), 0)
+        
+    def test_binary_to_octal(self):
+        self.assertEqual(binary_to_octal('101010'), '52')
+        self.assertEqual(binary_to_octal('11111111'), '377')
+        self.assertEqual(binary_to_octal('0'), '0')
+        
+    def test_binary_to_hexadecimal(self):
+        self.assertEqual(binary_to_hexadecimal('101010'), '2A')
+        self.assertEqual(binary_to_hexadecimal('11111111'), 'FF')
+        self.assertEqual(binary_to_hexadecimal('0'), '0')
+        
+    def test_decimal_to_binary(self):
+        self.assertEqual(decimal_to_binary(42), '101010')
+        self.assertEqual(decimal_to_binary(255), '11111111')
+        self.assertEqual(decimal_to_binary(0), '0')
+        
+    def test_decimal_to_octal(self):
+        self.assertEqual(decimal_to_octal(42), '52')
+        self.assertEqual(decimal_to_octal(255), '377')
+        self.assertEqual(decimal_to_octal(0), '0')
+        
+    def test_decimal_to_hexadecimal(self):
+        self.assertEqual(decimal_to_hexadecimal(42), '2A')
+        self.assertEqual(decimal_to_hexadecimal(255), 'FF')
+        self.assertEqual(decimal_to_hexadecimal(0), '0')
+        
+    def test_octal_to_decimal(self):
+        self.assertEqual(octal_to_decimal('52'), 42)
+        self.assertEqual(octal_to_decimal('377'), 255)
+        self.assertEqual(octal_to_decimal('0'), 0)
+        
+    def test_octal_to_binary(self):
+        self.assertEqual(octal_to_binary('52'), '101010')
+        self.assertEqual(octal_to_binary('377'), '11111111')
+        self.assertEqual(octal_to_binary('0'), '0')
+        
+    def test_octal_to_hexadecimal(self):
+        self.assertEqual(octal_to_hexadecimal('52'), '2A')
+        self.assertEqual(octal_to_hexadecimal('377'), 'FF')
+        self.assertEqual(octal_to_hexadecimal('0'), '0')
+        
+    def test_hexadecimal_to_decimal(self):
+        self.assertEqual(hexadecimal_to_decimal('2A'), 42)
+        self.assertEqual(hexadecimal_to_decimal('FF'), 255)
+        self.assertEqual(hexadecimal_to_decimal('0'), 0)
+        
+    def test_hexadecimal_to_binary(self):
+        self.assertEqual(hexadecimal_to_binary('2A'), '101010')
+        self.assertEqual(hexadecimal_to_binary('FF'), '11111111')
+        self.assertEqual(hexadecimal_to_binary('0'), '0')
+        
+    def test_hexadecimal_to_octal(self):
+        self.assertEqual(hexadecimal_to_octal('2A'), '52')
+        self.assertEqual(hexadecimal_to_octal('FF'), '377')
+        self.assertEqual(hexadecimal_to_octal('0'), '0')
 
 
 if __name__ == '__main__':
